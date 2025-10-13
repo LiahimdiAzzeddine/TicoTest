695da01f2b38d0dd0a3e2f7761137abf0936c1b4
Stifffller <100485595+Stifffller@users.noreply.github.com>
2025-10-13 12:54:19 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import { SelectedIndexProvider } from './contexts/SelectedIndexProvider';
 import Inscription from './pages/Inscription';
 import AtHome from './pages/AtHome';
 import Cart from './pages/Cart';
-import AtSchool from './pages/atSchool';
+import AtSchool from './pages/AtSchool';
 import Organizations from './pages/organizations';
 import Organization from './pages/Organization';
 import { CartProvider } from './contexts/CartContext';
